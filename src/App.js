ea6ea7ff5a940f65c1b703726601a67a0feb05b2
Ujala <154305801+Ujala9@users.noreply.github.com>
2025-06-27 06:15:10 +0000

    updated event filter UI

--- src/App.js
+++ src/App.js
@@ -39,8 +39,10 @@ export default function App() {
   return (
     <>
     <Header/>
+    
+    <h1 className="text-success text-center display-1 fw-bold py-2">MeetUp Events</h1>
 
-    <div className="d-flex justify-content-end px-5 mt-4 mx-4">
+    <div className="d-flex justify-content-end px-5 mx-5 pb-3">
      <select className="form-select"
         onChange={(e) => setFilterType(e.target.value)} value={filterType} style={{ maxWidth: "200px" }}>
           <option value="">Select event Type</option>
@@ -49,9 +51,7 @@ export default function App() {
        </select>
        </div>
 
-     {/* <div className=""> */}
-      <h1 className="text-success text-center display-1 fw-bold my-4 py-2">MeetUp Events</h1>
-      {/* </div> */}
+     
        
       
       <section className="container">
